36f154ca2f770649d5b726b5e82ef86546701a40
Weerapat T <45629031+weerapata1@users.noreply.github.com>
2021-02-12 21:10:59 +0700

    upDateEditBook

--- src/components/EditEbook.vue
+++ src/components/EditEbook.vue
@@ -1,6 +1,26 @@
 <template>
     <div id="container">
         <div class="display-4 text-center">Edit EBook</div>
+        <div class="row text-center">
+            <div class="col col-sm-6 col-12 mx-auto mt-3">
+                <div class="card-header">
+                    <div class="card-title">ID : {{data.id}}</div>
+                </div>
+                <div class="card-body">
+                    <div class="display-3 text-primary">
+                        Name : {{data.name}}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col col-sm-6 col-12 mx-auto mt-3">
+                <button class="btn btn-primary btn-sm mr-2" @click="onHome()">Home</button>  
+                <button class="btn btn-primary btn-sm" @click="onAbout()">About</button>
+            </div>
+            
+        </div>
+        
     </div>
 </template>
 <script src="./editEbook.js"></script>
--- src/components/editEbook.js
+++ src/components/editEbook.js
@@ -1,3 +1,20 @@
 export default {
-    name : 'EditEbook'
+    name : 'EditEbook',
+    data(){
+        return {
+            data : this.$route.params.data
+        };
+    },
+    methods :{
+        onHome(){
+            this.$router.push("/");
+
+        },
+        onAbout(){
+            this.$router.push("/about");
+
+        }
+
+    }
+
 }
