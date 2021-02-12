697c1b8446ca23693c9e35d54ddd1b90b644ab85
Weerapat T <45629031+weerapata1@users.noreply.github.com>
2021-02-12 21:10:37 +0700

    upDateBook

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,10 +1,24 @@
 <template>
     <div id="container">
         <div class="display-4 text-center">All EBooks</div> 
-        
+        <div class="row">
+              <div class="col-12 col-sm-6 col-lg-3 mt-4" v-for="book in bookLists" v-bind:key="book.id" >
+                <div class="card">
+                    <div class="card-header">
+                        ID : {{book.id}}
+                    </div>
+                    <div class="card-body">
+                        <Ebook :ebookObject="book" />
+                        </div> 
+                    </div>
+                
+            </div> 
+            
+        </div>
     </div>
-    
 </template>
+
+
 <script src="./books.js"></script>
 
 
--- src/components/books.js
+++ src/components/books.js
@@ -1,7 +1,23 @@
+import Ebook from '../components/Ebook.vue'
+
 export default {
     name : 'books',
+    components : {
+        Ebook
+    },
     data(){
-
-        return {};
+        return {
+            bookLists : [
+                { id : "0001" , name : "Vue.js"},
+                { id : "0002" , name : "Angular"},
+                { id : "0003" , name : "React"},
+                { id : "0004" , name : "Node.Js"},
+                { id : "0005" , name : "PHP"},
+                { id : "0006" , name : "Linux"},
+            ],
+        };
+    },
+    methods:{
+        
     }
 }
